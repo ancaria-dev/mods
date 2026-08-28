f0887a7e32c6ea4cf54f81a5cb8c78df03d797a0
MairwunNx <mairwunnx@gmail.com>
2026-08-29 00:58:20 +0300

    Configure Old Huge Potions mod

---
+++ old-huge-potions/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("dev.ancaria.coderpack")
+}
+
+version = "0.1.0"
+
+sacred {
+    id = "old-huge-potions"
+    displayName = "Old Huge Potions"
+    description = "Restores Sacred’s original potion system by turning every potion you pick up into the full-size version of its kind."
+    entrypoint = "dev.ancaria.potions.PotionsMod"
+    author("MairwunNx (Pavel Erokhin)")
+    website = "https://ancaria.dev"
+    repository = "https://github.com/ancaria-dev/mods"
+    apiVersion = "0.1.0"
+    installTo = layout.dir(providers.gradleProperty("sacredDir").map { file("$it/mods") })
+}
